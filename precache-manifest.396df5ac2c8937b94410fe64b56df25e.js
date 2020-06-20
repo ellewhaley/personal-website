7b642f5da0f3418f093dcee46d29d3a5ffe2cae7
Elle Whaley <elliewhaley11@gmail.com>
2020-06-20 16:03:49 +0100

    Updates

---
+++ precache-manifest.396df5ac2c8937b94410fe64b56df25e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498c576b63f8d1a4ad0d68c35d0c1d79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d2b7d6906615d591ca2",
+    "url": "/static/css/main.f71e88d9.chunk.css"
+  },
+  {
+    "revision": "e3ffe04aa92ba909a227",
+    "url": "/static/js/2.f3a6956a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f3a6956a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2b7d6906615d591ca2",
+    "url": "/static/js/main.700f5f0e.chunk.js"
+  },
+  {
+    "revision": "4ac598fa20d7619937c8",
+    "url": "/static/js/runtime-main.90834230.js"
+  },
+  {
+    "revision": "0a4a6acc15354d6d63c5b9946c63fd23",
+    "url": "/static/media/1.8e5c2792.png"
+  },
+  {
+    "revision": "3f8bb42cbddf05ce284a2d418497dcae",
+    "url": "/static/media/3.efed6054.png"
+  },
+  {
+    "revision": "3cd256158ec082c05b2c422d650a81bc",
+    "url": "/static/media/6.ab07cbfa.png"
+  },
+  {
+    "revision": "87e1e36518fc142e0147d1473c8e55ca",
+    "url": "/static/media/background.924b607e.png"
+  },
+  {
+    "revision": "19196717f8d23777d4b1f09427636e03",
+    "url": "/static/media/bynd.34e00d9b.png"
+  },
+  {
+    "revision": "3391b96156844fe52abc577bc92789bc",
+    "url": "/static/media/game.405dd2f0.png"
+  },
+  {
+    "revision": "aab80e2587323b4414b6613695879364",
+    "url": "/static/media/github.aab80e25.svg"
+  },
+  {
+    "revision": "bb37339a6a55a19ec648e39b698e493d",
+    "url": "/static/media/inbetweenus.aa74df36.png"
+  },
+  {
+    "revision": "8909ed986de2cbecde47807323ac8832",
+    "url": "/static/media/linkedin.8909ed98.svg"
+  },
+  {
+    "revision": "fdeaf839e7e31abf83682817118246e2",
+    "url": "/static/media/mail1.fdeaf839.svg"
+  },
+  {
+    "revision": "48c9ef27bfc3d93a921a28f6f46a58f4",
+    "url": "/static/media/reed.4c435b1f.png"
+  },
+  {
+    "revision": "e1c63bc989d7e483470535f6537dc62a",
+    "url": "/static/media/twitter.e1c63bc9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31edfcc12f9c6a5d776aaa3e0616a761.js"
+  "/precache-manifest.396df5ac2c8937b94410fe64b56df25e.js"
 );
 
 self.addEventListener('message', (event) => {
