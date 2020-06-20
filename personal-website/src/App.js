682185f364a7c8cad49d7accf2891a67b4494ff2
Elle Whaley <elliewhaley11@gmail.com>
2020-06-20 11:44:54 +0100

    added content to page

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import ThreeContainer from './components/ThreeContainer';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <ThreeContainer />
       <Main />
+      <Footer />
     </div>
   );
 }
